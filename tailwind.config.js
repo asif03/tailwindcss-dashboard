9fd1245e0d3a5734c0c0a16a3949cddefe780cb3
asif03 <babu033045@gmail.com>
2023-08-12 07:10:40 +0600

    Login form done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0d6efd',
+        secondary: '#adb5bd',
+      },
+      fontFamily: {
+        roboto: ['Roboto'],
+      },
+      animation: {
+        introX: '.4s intro-x-animation ease-in-out .33333s',
+        introXReverse: '.4s intro-reverse-x-animation ease-in-out .33333s',
+      },
+      keyframes: {
+        'intro-x-animation': {
+          '0%': {
+            transform: 'translate(-50px)',
+            opacity: '0',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translate(0)',
+          },
+        },
+        'intro-reverse-x-animation': {
+          '0%': {
+            transform: 'translate(50px)',
+            opacity: '0',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translate(0)',
+          },
+        },
+        'intro-menu': {
+          '0%': {
+            transform: 'translate(50px)',
+            opacity: '0',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translate(0)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
