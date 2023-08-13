ca240445ac4b2fbaea4cfc5c76c35399eca8b845
Asif <babu033045@gmail.com>
2023-08-13 14:05:13 +0600

    Update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{html,js}'],
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0d6efd',
-        secondary: '#adb5bd',
+        primary: "#0d6efd",
+        secondary: "#adb5bd",
       },
       fontFamily: {
-        roboto: ['Roboto'],
+        roboto: ["Roboto"],
+        "font-awesome": ["FontAwesome"],
       },
       animation: {
-        introX: '.4s intro-x-animation ease-in-out .33333s',
-        introXReverse: '.4s intro-reverse-x-animation ease-in-out .33333s',
+        introX: ".4s intro-x-animation ease-in-out .33333s",
+        introXReverse: ".4s intro-reverse-x-animation ease-in-out .33333s",
       },
       keyframes: {
-        'intro-x-animation': {
-          '0%': {
-            transform: 'translate(-50px)',
-            opacity: '0',
+        "intro-x-animation": {
+          "0%": {
+            transform: "translate(-50px)",
+            opacity: "0",
           },
-          '100%': {
+          "100%": {
             opacity: 1,
-            transform: 'translate(0)',
+            transform: "translate(0)",
           },
         },
-        'intro-reverse-x-animation': {
-          '0%': {
-            transform: 'translate(50px)',
-            opacity: '0',
+        "intro-reverse-x-animation": {
+          "0%": {
+            transform: "translate(50px)",
+            opacity: "0",
           },
-          '100%': {
+          "100%": {
             opacity: 1,
-            transform: 'translate(0)',
+            transform: "translate(0)",
           },
         },
-        'intro-menu': {
-          '0%': {
-            transform: 'translate(50px)',
-            opacity: '0',
+        "intro-menu": {
+          "0%": {
+            transform: "translate(50px)",
+            opacity: "0",
           },
-          '100%': {
+          "100%": {
             opacity: 1,
-            transform: 'translate(0)',
+            transform: "translate(0)",
           },
         },
       },
